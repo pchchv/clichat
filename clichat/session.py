62a7d0e575fbf7658451a1ecec60e9e25bfcb641
pchchv <ipchchv@gmail.com>
2023-04-03 01:04:05 +0300

    Implement a rename_session func

--- clichat/session.py
+++ clichat/session.py
@@ -24,3 +24,19 @@ def delete_session(session):
         return f"session {session} does not exist"
 
     os.unlink(session_path)
+
+
+def rename_session(session, newname):
+    """Renames session.
+    Returns None on success, error string otherwise"""
+    session_path = storage.get_session_path(session, True)
+    if not session_path:
+        return f"session {session} does not exist"
+
+    new_session_path = storage.get_session_path(newname, True)
+    if new_session_path:
+        return f"session {newname} already exists"
+
+    new_session_path = storage.get_session_path(newname)
+
+    os.rename(session_path, new_session_path)
