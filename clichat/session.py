0836a3e0dee70f28eab52b86ed3140976fdd72f4
pchchv <ipchchv@gmail.com>
2023-04-03 01:05:00 +0300

    Refactoring

--- clichat/session.py
+++ clichat/session.py
@@ -4,9 +4,10 @@ import glob
 from clichat import storage
 
 
-def list_sessions():
+def list():
     """List names of sessions."""
     sess_paths = glob.glob(os.path.join(storage.get_cache_path(), "*.yaml"))
+
     return sorted(
         [
             re.sub("\.yaml\Z", "", os.path.basename(sess_path))  # noqa: W605
@@ -15,18 +16,17 @@ def list_sessions():
     )
 
 
-def delete_session(session):
+def delete(session):
     """Deletes a session.
     Returns None on success, error string otherwise"""
     session_path = storage.get_session_path(session, True)
-
     if not session_path:
         return f"session {session} does not exist"
 
     os.unlink(session_path)
 
 
-def rename_session(session, newname):
+def rename(session, newname):
     """Renames session.
     Returns None on success, error string otherwise"""
     session_path = storage.get_session_path(session, True)
