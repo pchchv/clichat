6eaa8282164ea6c6aabe05f49bf06fc4cb056be9
pchchv <ipchchv@gmail.com>
2023-04-03 12:19:33 +0300

    Implement a DotDict class

--- clichat/utils.py
+++ clichat/utils.py
@@ -1,2 +1,10 @@
 CONSOLE_DEBUG_LOGGING = False
 scratch_session = "last"
+
+
+class DotDict(dict):
+    """dot.notation access to dictionary attributes"""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
