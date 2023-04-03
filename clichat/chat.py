f53e84f91be289723d9368a2c1539f378907c359
pchchv <ipchchv@gmail.com>
2023-04-03 12:37:12 +0300

    Implement a query_chatgpt func

--- clichat/chat.py
+++ clichat/chat.py
@@ -1,6 +1,18 @@
 import os
 import yaml
+import types
+import openai
 import collections
+import openai.error
+from clichat import utils, errors
+
+
+DEFAULT_OPENAI_SETTINGS = {
+    "model": "gpt-3.5-turbo",
+    "temperature": 0.1,
+    "n": 1,
+    "stream": False,
+}
 
 
 class Message(collections.namedtuple("Message", ["role", "content"])):
@@ -21,6 +33,9 @@ class Message(collections.namedtuple("Message", ["role", "content"])):
         Creating a class instance from the provided YAML representation.
         """
         return cls(**seq)
+
+
+yaml.add_representer(Message, Message.represent_for_yaml)
 
 
 def set_azure_if_present(config):
@@ -46,3 +61,26 @@ def map_single(result):
     """Maps a result to a Message."""
     response_message = [choice["message"] for choice in result["choices"]][0]
     return Message(response_message["role"], response_message["content"])
+
+
+def query_chatgpt(messages, config):
+    """Queries the chat GPT API with the given messages and config."""
+    openai.api_key = config["openai_api_key"]
+    config = utils.merge_dicts(DEFAULT_OPENAI_SETTINGS, config)
+    set_azure_if_present(config)
+    dict_messages = [msg._asdict() for msg in messages]
+
+    try:
+        result = openai.ChatCompletion.create(messages=dict_messages, **config)
+        if isinstance(result, types.GeneratorType):
+            return map_generator(result)
+        elif isinstance(result, dict):
+            return map_single(result)
+        else:
+            raise ValueError(f"unexpected result openai: {result}")
+    except (
+        openai.error.InvalidRequestError,
+        openai.error.AuthenticationError,
+        openai.error.RateLimitError,
+    ) as e:
+        raise errors.CliChatError(f"openai error: {e}")
